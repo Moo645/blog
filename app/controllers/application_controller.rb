6e0798d5d8c338477406df38b780fa0807e9e904
wells <wells.liu@neutron-rubber.com>
2023-11-03 04:03:18 +0800

    feat: 修改幾個erb, 讓畫面看起來沒那麼難過

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   private
 
   def list_all_notebooks_and_categories
-    @list_of_notebooks = Notebook.includes(:note_categories).all
+    @list_of_notebooks = Notebook.includes(note_categories: :notes).all
   end
 end
